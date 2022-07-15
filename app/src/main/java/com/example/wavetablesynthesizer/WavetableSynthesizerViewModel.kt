57e51573ecc91d41500a60d778f0a8051f03af15
Jan Wilczek <janekw@vp.pl>
2022-07-15 22:43:52 +0200

    Removed unused import.

--- app/src/main/java/com/example/wavetablesynthesizer/WavetableSynthesizerViewModel.kt
+++ app/src/main/java/com/example/wavetablesynthesizer/WavetableSynthesizerViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 
 class WavetableSynthesizerViewModel(
   private val wavetableSynthesizer: WavetableSynthesizer
