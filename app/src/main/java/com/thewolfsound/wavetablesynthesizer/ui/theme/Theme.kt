b9ab8a05b3c0428efc1e6e317c6dbf06c5254a1c
Jan Wilczek <janekw@vp.pl>
2022-07-23 15:12:18 +0200

    Added WolfSound colors.

--- app/src/main/java/com/thewolfsound/wavetablesynthesizer/ui/theme/Theme.kt
+++ app/src/main/java/com/thewolfsound/wavetablesynthesizer/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = WolfSoundOrange,
+    primaryVariant = WolfSoundDarkOrange,
+    secondary = WolfSoundGray
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = WolfSoundOrange,
+    primaryVariant = WolfSoundDarkOrange,
+    secondary = WolfSoundGray
 
     /* Other default colors to override
     background = Color.White,
